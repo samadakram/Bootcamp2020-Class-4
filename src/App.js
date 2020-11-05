4a58f142f455755a1292ea09f867e71cf47dcf7a
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-05 20:42:13 +0500

    Added a component named Counter and Shifed Counter Jsx in Counter.js and passes value using props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+import Counter from './Counter';
 
 function App(){
 
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(0)
 
   return(
     <div>
-      <h1>Value of counter is: {count} </h1>
+      <Counter process={count} />
       <button onClick={()=> setCount(++count)}>Increment</button>
       <button onClick={()=> setCount(--count)}>Decrement</button>
       <button onClick={()=> setCount(0)}>Reset</button>
---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Counter(props){
+    return(
+        <div>
+            <h1>Value of Counter is: {props.process} </h1>
+        </div>
+    );
+}
+
+export default Counter;
