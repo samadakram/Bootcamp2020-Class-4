90f84860b61b33897aae729e8c53cc35fe93aa10
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-05 20:20:57 +0500

    Update App.js
    again coded from scratch and this added a Reset funtionality by clicking on Reset button the value will zero

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App(){
 
   return(
     <div>
-      <h1>Value of counter is: {count}</h1>
-      <button onClick={() => setCount(++count)}>Increment</button>
-      <button onClick={() => setCount(--count)}>Decrement</button>
+      <h1>Value of counter is: {count} </h1>
+      <button onClick={()=> setCount(++count)}>Increment</button>
+      <button onClick={()=> setCount(--count)}>Decrement</button>
+      <button onClick={()=> setCount(0)}>Reset</button>
     </div>
   );
 }
