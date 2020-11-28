613cd4da23418c36640109cfe914231855de3a01
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-28 18:03:20 +0500

    Update StatePactice.js

--- src/Components/StatePactice.js
+++ src/Components/StatePactice.js
@@ -5,20 +5,42 @@ export const StatePractice = () => {
     // const [name, setName] = useState("Samad Akram");
     // const [email, setEmail] = useState("samadakram@gmailcom");
 
-    const [arr, setArr] = useState(["Car"]);
+    // const [arr, setArr] = useState(["Car"]);
+    const [obj, setObj] = useState({
+        name: "Akram Khan",
+        email: "akram@gmail.com"
+    });
 
     const handleClick = () => {
+        //Simple State
         // setName("Abdul Samad");
         // setEmail("abdul@gmail.com");
-        setArr([...arr,"Bike","Truck"]);
+
+        // Calling Arayy in State
+        // setArr([...arr,"Bike","Truck"]);
+
+        // Calling Object in state
+        setObj({
+            name: "Abdul Samad",
+            email: "samad@gmail.com"
+        });
+
+
     }
 
     return (
         <div>
+            {/* Simple State */}
             {/* <h1>{name}</h1>
             <h2>{email}</h2> */}
 
-            <h3>{ arr }</h3>
+            {/* Array Example */}
+            {/* <h3>{ arr }</h3> */}
+
+
+            {/* Object Example */}
+            <h2>{obj.name}</h2>
+            <h3>{ obj.email }</h3>
 
 
 
