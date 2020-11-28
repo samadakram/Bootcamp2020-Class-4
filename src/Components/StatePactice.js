4e9985852f71d14082d47eb62c8a202973cf76a0
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-28 17:49:09 +0500

    Update StatePactice.js

--- src/Components/StatePactice.js
+++ src/Components/StatePactice.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const StatePractice = ()=> {
-    return(
+export const StatePractice = () => {
+
+    const [name, setName] = useState("Samad Akram");
+    const [email, setEmail] = useState("samadakram@gmailcom");
+
+    return (
         <div>
-            <h1>React Hooks</h1>
+            <h1>{name}</h1>
+            <h2>{email}</h2>
+            <button onClick={()=> setName("Abdul Samad")} >Update Name</button>
+            <button onClick={()=> setEmail("abdul@gmail.com")} >Update Email</button>
         </div>
     );
 }
