254344713aebda6b4c01d90ad16aab363ac604c1
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-28 17:51:18 +0500

    Update StatePactice.js

--- src/Components/StatePactice.js
+++ src/Components/StatePactice.js
@@ -5,12 +5,16 @@ export const StatePractice = () => {
     const [name, setName] = useState("Samad Akram");
     const [email, setEmail] = useState("samadakram@gmailcom");
 
+    const handleClick = () => {
+        setName("Abdul Samad");
+        setEmail("abdul@gmail.com");
+    }
+
     return (
         <div>
             <h1>{name}</h1>
             <h2>{email}</h2>
-            <button onClick={()=> setName("Abdul Samad")} >Update Name</button>
-            <button onClick={()=> setEmail("abdul@gmail.com")} >Update Email</button>
+            <button onClick={handleClick} >Update</button>
         </div>
     );
 }
