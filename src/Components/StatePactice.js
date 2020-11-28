d8a052edc0557a0885fbe223066df9e3a6bfc771
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-28 17:57:08 +0500

    Update StatePactice.js

--- src/Components/StatePactice.js
+++ src/Components/StatePactice.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 
 export const StatePractice = () => {
 
-    const [name, setName] = useState("Samad Akram");
-    const [email, setEmail] = useState("samadakram@gmailcom");
+    // const [name, setName] = useState("Samad Akram");
+    // const [email, setEmail] = useState("samadakram@gmailcom");
+
+    const [arr, setArr] = useState(["Car"]);
 
     const handleClick = () => {
-        setName("Abdul Samad");
-        setEmail("abdul@gmail.com");
+        // setName("Abdul Samad");
+        // setEmail("abdul@gmail.com");
+        setArr([...arr,"Bike","Truck"]);
     }
 
     return (
         <div>
-            <h1>{name}</h1>
-            <h2>{email}</h2>
+            {/* <h1>{name}</h1>
+            <h2>{email}</h2> */}
+
+            <h3>{ arr }</h3>
+
+
+
             <button onClick={handleClick} >Update</button>
         </div>
     );
